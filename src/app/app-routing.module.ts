4085fd59b3743f5bd331489eb05e6f7e61b79e78
Erikson Mizael <erikson_mizael@hotmail.com>
2021-09-03 13:36:51 -0300

    Adicionando views e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './content/messages/error/not-found/not-found.component';
+import { ConnectionComponent } from './content/pages/Connection/Connection.component';
+import { HomeComponent } from './content/pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'login',
+    component: ConnectionComponent
+  },
+
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/content/pages/Connection/Connection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Connection',
+  templateUrl: './Connection.component.html',
+  styleUrls: ['./Connection.component.scss']
+})
+export class ConnectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
